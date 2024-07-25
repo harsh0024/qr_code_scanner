e7e49b9dfb5cc039f503d3972f76239bce6f0b6c
Harsh Chachane <131657632+harsh0024@users.noreply.github.com>
2024-07-26 00:07:53 +0530

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,1 +1,72 @@
+import 'package:flutter/material.dart';
 
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = ''; // Initialize _username
+  String _password = ''; // Initialize _passwor
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _username = value!,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value?.isEmpty?? true) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value!,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: (){
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    // Use _username and _password for login
+                    if (_username == 'cradmin' && _password == 'crpassword') {
+                      // Login successful, navigate to the next screen
+                      Navigator.pushReplacementNamed(context, '/home');
+                    } else {
+                      // Login failed, show an error message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Invalid username or password')),
+                      );
+                    }
+                  }
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
